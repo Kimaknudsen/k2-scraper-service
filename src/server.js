95ded657c220baa3e3f7db9a3b5dd8711ded1c13
Kimaknudsen <post@k2trafikkskole.no>
2026-03-18 21:33:41 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -89,5 +89,5 @@ app.get("/validate-cookies", auth, async (_req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`K2 Scraper running on :${PORT}`));
